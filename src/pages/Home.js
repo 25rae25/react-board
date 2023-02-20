9ba0391cc2755169ef63e2f46c3e260a19b5ef62
25rae25 <whdudfo0124@gmail.com>
2023-02-20 16:50:23 +0900

    영화게시판

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+	return(
+		<div>
+			<h1>홈페이지입니다.</h1>
+			<p>라우터 실습을 위하여 만들어보자.</p>
+		</div>
+	);
+};
+
+export default Home;
---
+++ src/pages/Movies.js
@@ -0,0 +1,9 @@
+const Movies = () => {
+	return(
+		<div>
+			<h1>넷플릭스 영화 추천 목록</h1>
+		</div>
+	)
+};
+
+export default Movies;
